20240c33cee5b3b4bf556d9ec0a2b54422e990c4
horsefacegirl <lindstrmida@gmail.com>
2019-10-20 18:32:59 +0200

    add css, html

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -1,55 +1,48 @@
-const pizza1 = "Vegetarian Pizza"
-const pizza2 = "Hawaiian Pizza"
-const pizza3 = "Pepperoni Pizza"
+const startOrdering = () => {
+  const pizza1 = "Vegetarian Pizza";
+  const pizza2 = "Hawaiian Pizza";
+  const pizza3 = "Pepperoni Pizza";
 
-const pizzaPrice = 80
+  const pizzaPrice = 80;
 
-alert(`Hey! Happy to serve your pizza. On our menu we have
+  alert(`Hey! Happy to serve your pizza. On our menu we have
 ${pizza1}, ${pizza2}, ${pizza3}`);
 
-let orderName = prompt(`Enter the name of the pizza you want to order today. On our menu we have
+  let orderName = prompt(`Enter the name of the pizza you want to order today. On our menu we have
 ${pizza1}, ${pizza2}, ${pizza3}`);
 
-const checkOrderName = (orderNameInput) => {
-    return orderNameInput === pizza1 || orderNameInput === pizza2 || orderNameInput === pizza3
-  }
-  // Sometimes the argument name can have the same name as the variable outside the function, 
-  // but JS knows to distinguish and will ignore the outside variable. Makes it confusing to use same names.
+  const checkOrderName = orderNameInput => {
+    return (
+      orderNameInput === pizza1 ||
+      orderNameInput === pizza2 ||
+      orderNameInput === pizza3
+    );
+  };
 
-//  const checkOrderName = orderName => {
-// if (orderName === pizza1 || orderName === pizza2 || orderName === pizza3) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-
-// Set the variable outside the function.
-// You want to fill null with something - userinput.
-let orderQuantity = null
-
-if (checkOrderName(orderName)) {
-  orderQuantity = prompt(`How many ${orderName} do you want?`)
-} else {
-  alert("This pizza is not on the menu!")
-    //With return it will not read further.
-    //Nice to do: Later change - keep asking user for correct name?
-}
-
-const totalCost = (orderQuantityInput) => {
-  return orderQuantityInput * pizzaPrice
-}
-
-const cookingTime = (orderQuantityInput) => {
-  if (orderQuantityInput === 1 || orderQuantityInput === 2) {
-    return 10;
-  } else if (orderQuantityInput >= 3 && orderQuantityInput <= 5) {
-    return 15;
+  let orderQuantity = null;
+  if (checkOrderName(orderName)) {
+    orderQuantity = prompt(`How many ${orderName} do you want?`);
   } else {
-    return 20;
+    alert("This pizza is not in the menu!");
   }
-}
 
-alert(
-  `Great, I ’ll get started on your ${orderName} right away, it will cost you ${totalCost(
-      orderQuantity
-    )} kr. It will take ${cookingTime(orderQuantity)} minutes`)
+  const totalCost = orderQuantityInput => {
+    return orderQuantityInput * pizzaPrice;
+  };
+
+  const cookingTime = orderQuantityInput => {
+    if (orderQuantityInput === 1 || orderQuantityInput === 2) {
+      return 10;
+    } else if (orderQuantity > 2 && orderQuantity < 6) {
+      return 15;
+    } else {
+      return 20;
+    }
+  };
+
+  document.getElementById("order-button").style.display = "none";
+
+  document.getElementById("order-confirmation").innerHTML = `Great, I'll get started on your ${orderName} right away, it will cost you ${totalCost(orderQuantity)} kr. It will take ${cookingTime(orderQuantity)} minutes. See you soon!`;
+};
+
+document.getElementById("order-button").onclick = startOrdering;
